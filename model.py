d434f444bff1490a02e7a4f4810cf926e85dde7e
Patrick Döring <Patrick.Doering@harman.com>
2018-04-20 15:44:21 +0200

    Improved model by using all 3 cameras, augmented data, cropped images and LeNet model

--- model.py
+++ model.py
@@ -11,26 +11,44 @@ with open('../data/driving_log.csv') as csvfile:
 images = []
 measurements = []
 for line in lines:
-   source_path = line[0]
-   filename = source_path.split('/')[-1]
-   current_path = '../data/IMG/' + filename
-   image = cv2.imread(current_path)
-   images.append(image)
-   measurement = float(line[3])
-   measurements.append(measurement)
+   for i in range(3):
+   	source_path = line[i]
+   	filename = source_path.split('/')[-1]
+   	current_path = '../data/IMG/' + filename
+   	image = cv2.imread(current_path)
+   	images.append(image)
+   	measurement = float(line[3])
+   	measurements.append(measurement)
+
+augmented_images, augmented_measurements = [], []
+for image, measurement in zip(images, measurements):
+   augmented_images.append(image)
+   augmented_measurements.append(measurement)
+   augmented_images.append(cv2.flip(image,1))
+   augmented_measurements.append(measurement*-1.0)
    
-X_train = np.array(images)
-y_train = np.array(measurements)
+X_train = np.array(augmented_images)
+y_train = np.array(augmented_measurements)
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense
+from keras.layers import Flatten, Dense, Lambda
+from keras.layers import Convolution2D
+from keras.layers.pooling import MaxPooling2D 
+from keras.layers import Cropping2D
 
 model = Sequential()
-model.add(Flatten(input_shape=(160,320,3)))
+model.add(Lambda(lambda x: x /255 - 0.5, input_shape=(160,320,3)))
+model.add(Cropping2D(cropping=((70,25), (0,0))))
+model.add(Convolution2D(6,5,5,activation="relu"))
+model.add(MaxPooling2D())
+model.add(Convolution2D(6,5,5,activation="relu"))
+model.add(MaxPooling2D())
+model.add(Flatten())
+model.add(Dense(120))
+model.add(Dense(84))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
 model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=5)
 
 model.save('model.h5')
-
