a6bf2b1f05620566dc7819d92375474336663fa4
Patrick <patrick.dring@googlemail.com>
2018-04-28 17:58:30 +0200

    Separeted model resp. cnn layer visu. into own python modul. Created some figures for writeup docu.

--- model.py
+++ model.py
@@ -1,4 +1,3 @@
-import matplotlib.pyplot as plt
 import cv2
 import numpy as np
 import csv
@@ -14,7 +13,6 @@ from keras.optimizers import Adam
 from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
 
-from keras.utils.vis_utils import plot_model
 from keras import backend as K
 from keras.utils import np_utils
 
@@ -25,14 +23,14 @@ def load_driving_log(file):
         for line in reader:
             lines.append(line)
     # skip first line/header
-    return lines[1:]  
+    return lines[1:]
 
 def load_driving_data(log_path, data_path):
     path = data_path
     lines = log_path
 
     shuffle(lines)
-    
+
     images = []
     angles = []
 
@@ -57,7 +55,7 @@ def load_driving_data(log_path, data_path):
 
     augmented_images = []
     augmented_angles = []
-    
+
     for (image, angle) in zip(images, angles):
         augmented_images.append(image)
         augmented_angles.append(angle)
@@ -84,7 +82,7 @@ model.add(Cropping2D(cropping=((50,20), (0,10)), input_shape=(160,320,3)))
 # Preprocess incoming data, centered around zero with small standard deviation
 model.add(Lambda(lambda x: (x / 255.0) - 0.5))
 
-#Nvidia model
+# Nvidia model
 model.add(Conv2D(24, (5, 5), activation="relu", name="conv_1", strides=(2, 2)))
 model.add(Conv2D(36, (5, 5), activation="relu", name="conv_2", strides=(2, 2)))
 model.add(Conv2D(48, (5, 5), activation="relu", name="conv_3", strides=(2, 2)))
@@ -104,34 +102,10 @@ model.add(Dense(10, activation='relu'))
 model.add(Dropout(.5))
 model.add(Dense(1))
 
-from kerastoolbox.visu import plot_feature_map
-
-plot_model(model, to_file='model_plot.png', show_shapes=True, show_layer_names=True)
-
 model.compile(loss='mse', optimizer='adam')
 
 model.summary()
 
-model.fit(X_train, y_train, validation_data=(X_valid, y_valid), batch_size=256, epochs=7, verbose=1)
-
-images = X_valid[:3]
-x = plot_feature_map(model, X=images, layer_id=3, n_columns=3, n=256)
-plt.show(x)
-x = plot_feature_map(model, X=images, layer_id=4, n_columns=3, n=256)
-plt.show(x)
-x = plot_feature_map(model, X=images, layer_id=5, n_columns=3, n=256)
-plt.show(x)
-x = plot_feature_map(model, X=images, layer_id=6, n_columns=3, n=256)
-plt.show(x)
-
-from kerastoolbox.visu import plot_weights
-x = plot_weights(model, layer_id=6, n=256, ax=None)
-plt.show(x)
-
-plot_model(model, to_file='model_plot.png', show_shapes=True, show_layer_names=True)
+model.fit(X_train, y_train, validation_data=(X_valid, y_valid), batch_size=2, epochs=7, verbose=1)
 
 model.save('model.h5')
-
-
-
-
---
+++ visualization.py
@@ -0,0 +1,82 @@
+import matplotlib.pyplot as plt
+import cv2
+import numpy as np
+import csv
+import tensorflow
+import keras
+
+from keras.models import Sequential
+from keras.layers import Flatten, Dense, Conv2D, ELU, Activation
+from keras.layers import Lambda, Dropout, Cropping2D, SpatialDropout2D
+
+from keras.optimizers import Adam
+
+from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
+
+from keras import backend as K
+from keras.utils import np_utils
+
+def create_model():
+    model = Sequential()
+
+    # trim image to only see section with road
+    model.add(Cropping2D(cropping=((50,20), (0,10)), input_shape=(160,320,3)))
+
+    # Preprocess incoming data, centered around zero with small standard deviation
+    model.add(Lambda(lambda x: (x / 255.0) - 0.5))
+
+    # Nvidia model
+    model.add(Conv2D(24, (5, 5), activation="relu", name="conv_1", strides=(2, 2)))
+    model.add(Conv2D(36, (5, 5), activation="relu", name="conv_2", strides=(2, 2)))
+    model.add(Conv2D(48, (5, 5), activation="relu", name="conv_3", strides=(2, 2)))
+    model.add(SpatialDropout2D(.5, dim_ordering='default'))
+    model.add(Conv2D(64, (3, 3), activation="relu", name="conv_4", strides=(1, 1)))
+    model.add(Conv2D(64, (3, 3), activation="relu", name="conv_5", strides=(1, 1)))
+
+    model.add(Flatten())
+
+    model.add(Dense(1164))
+    model.add(Dropout(.5))
+    model.add(Dense(100, activation='relu'))
+    model.add(Dropout(.5))
+    model.add(Dense(50, activation='relu'))
+    model.add(Dropout(.5))
+    model.add(Dense(10, activation='relu'))
+    model.add(Dropout(.5))
+    model.add(Dense(1))
+    return model
+
+
+model = create_model()
+model.load_weights('model.h5')
+
+from keras.utils.vis_utils import plot_model
+plot_model(model, to_file='./pics/nvidia_model.png', show_shapes=True, show_layer_names=True)
+
+images = []
+image = cv2.imread('./pics/center_2016_12_01_13_42_42_686.jpg')
+image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+images.append(image)
+
+image = cv2.imread('./pics/center_2016_12_01_13_45_31_341.jpg')
+image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+images.append(image)
+
+image = cv2.imread('./pics/center_2016_12_01_13_40_11_279.jpg')
+image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+images.append(image)
+
+from kerastoolbox.visu import plot_feature_map
+
+x = plot_feature_map(model, X=images, layer_id=2, n_columns=3, n=256)
+x.savefig('./pics/conv_1.png')
+x = plot_feature_map(model, X=images, layer_id=3, n_columns=3, n=256)
+x.savefig('./pics/conv_2.png')
+x = plot_feature_map(model, X=images, layer_id=4, n_columns=3, n=256)
+x.savefig('./pics/conv_3.png')
+x = plot_feature_map(model, X=images, layer_id=6, n_columns=3, n=256)
+x.savefig('./pics/conv_4.png')
+x = plot_feature_map(model, X=images, layer_id=7, n_columns=3, n=256)
+x.savefig('./pics/conv_5.png')
+
